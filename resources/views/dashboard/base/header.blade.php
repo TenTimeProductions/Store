4e850a2a6cd215699b45e0a31efb7ff17aed8752
CrazyAlien <pedro0192@hotmail.com>
2015-02-12 22:35:55 +0000

    Template ready to code some serious laravel shit

---
+++ resources/views/dashboard/base/header.blade.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bruno
+ * Date: 12/02/2015
+ * Time: 21:27
+ */
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js sidebar-large lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js sidebar-large lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js sidebar-large lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js sidebar-large"> <!--<![endif]-->
+
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+    <!-- BEGIN META SECTION -->
+    <meta charset="utf-8">
+    <title>Pixit - Responsive Boostrap3 Admin</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta content="" name="description" />
+    <meta content="themes-lab" name="author" />
+    <!-- END META SECTION -->
+    <!-- BEGIN MANDATORY STYLE -->
+    <link href="assets/css/icons/icons.min.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/plugins.min.css" rel="stylesheet">
+    <link href="assets/css/style.min.css" rel="stylesheet">
+    <!-- END  MANDATORY STYLE -->
+    <!-- BEGIN PAGE LEVEL STYLE -->
+    <link rel="stylesheet" href="assets/plugins/font-awesome-animation/font-awesome-animation.min.html">
+    <!-- END PAGE LEVEL STYLE -->
+    <script src="assets/plugins/modernizr/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+</head>
---
+++ resources/views/dashboard/master.blade.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bruno
+ * Date: 12/02/2015
+ * Time: 21:52
+ */
+
+@include('base.header');
+@include('base.sidebar-menu');
+@include('base.content');
+@include('features.chat');
+@include('base.footer');
