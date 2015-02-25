40a03c9af2b4ec3dd664ea434082c955e5b75ada
CrazyAlien <pedro0192@hotmail.com>
2015-02-25 23:20:27 +0000

    add login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,15 +4,18 @@
 
 	<script>
 		$(document).ready(function(){
-			$(".form").removeClass("loading");
+			setTimeout(function(){
+				$("#twowide").addClass("two wide column");
+				$("#fivewide").removeClass("six wide column").addClass("two wide column");
+				$(".form").removeClass("loading");
+			},500);
 		});
 	</script>
 
 @endsection
 @section('content')
-	<div class="ui grid">
-	<div class="row">
-		<div class="two wide column">
+	<div class="ui page grid">
+		<div id="twowide">
 		</div>
 		<div class="five wide column">
 			<div class="panel panel-default">
@@ -45,12 +48,27 @@
 								<div class="inline field">
 									<div class="ui checkbox">
 										<input type="checkbox" name="remember">
-										<label>Remember me?</label>
+										<label>Lembrar-me?</label>
 									</div>
 								</div>
 								<div class="ui submit button">Login</div>
-									<a href="/password/email">Forgot Your Password?</a>
+									<a href="/password/email">Esqueceu a sua password?</a>
 							</form>
+							<div class="field"></div>
+							<div class="field">
+								<div class="ui facebook button">
+									<i class="facebook icon"></i>
+
+								</div>
+								<div class="ui twitter button">
+									<i class="twitter icon	"></i>
+
+								</div>
+								<div class="ui google plus button">
+									<i class="google plus icon"></i>
+
+								</div>
+							</div>
 						</div>
 						<!--AQIIIIIIIIIIIIIIIIIIIIIIIIIIII-->
 				<!-- <div class="ui loading form segment">
@@ -92,6 +110,53 @@
 						</div>
 					</form> -->
 					</div>
+				</div>
+			</div>
+		<div id="fivewide" class="six wide column">
+			</div>
+			<div class="five wide column">
+				<div class="panel panel-default">
+					<div class="panel-heading">Register</div>
+					<div class="panel-body">
+						@if (count($errors) > 0)
+							<div class="alert alert-danger">
+								<strong>Whoops!</strong> There were some problems with your input.<br><br>
+								<ul>
+									@foreach ($errors->all() as $error)
+										<li>{{ $error }}</li>
+									@endforeach
+								</ul>
+							</div>
+						@endif
+				<div class="ui loading form segment">
+					<form class="form-horizontal" role="form" method="POST" action="/auth/login">
+						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+						<div class="field four-wide column">
+							<div class="field">
+								<label>Username</label>
+								<input placeholder="Username" type="text" name="email" value="{{ old('email') }}">
+							</div>
+							<div class="field">
+								<label>Email</label>
+								<input placeholder="Email" type="email" name="email" value="{{ old('email') }}">
+							</div>
+							<div class="field">
+								<label>Password</label>
+								<input type="password" name="password">
+							</div>
+							<div class="field">
+								<label>Verificar Password</label>
+								<input type="password" name="verify_password">
+							</div>
+						</div>
+						<div class="inline field">
+							<div class="ui checkbox">
+								<input type="checkbox" name="remember">
+								<label>Sou Vendedor</label>
+							</div>
+						</div>
+						<div class="ui submit button">Registar</div>
+					</form>
 				</div>
 			</div>
 		</div>
