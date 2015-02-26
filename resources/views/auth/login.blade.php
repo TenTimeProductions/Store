766f6ca507ddda34e6025ada294c56b687b00836
CrazyAlien <pedro0192@hotmail.com>
2015-02-26 01:22:54 +0000

    add login form animacoes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,61 +20,61 @@
 
 		<div class="five wide column">
 			<section class="wow slideInLeft" data-wow-duration="1s" data-wow-delay="0.2s">
-			<div class="panel panel-default">
-				<div class="panel-heading">Login</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
-					<!--aquiiiiiiiiiiiiiiiiiiiiiiiiiiiii-->
+					<div class="panel panel-default">
+					<div class="panel-heading">Login</div>
+					<div class="panel-body">
+						@if (count($errors) > 0)
+							<div class="alert alert-danger">
+								<strong>Whoops!</strong> There were some problems with your input.<br><br>
+								<ul>
+									@foreach ($errors->all() as $error)
+										<li>{{ $error }}</li>
+									@endforeach
+								</ul>
+							</div>
+						@endif
+						<!--aquiiiiiiiiiiiiiiiiiiiiiiiiiiiii-->
 
-							<div class="ui loading form segment">
-								<form class="form-horizontal" role="form" method="POST" action="/auth/login">
-									<input type="hidden" name="_token" value="{{ csrf_token() }}">
-									<div class="field four-wide column">
-										<div class="field">
-											<label>Email</label>
-											<input placeholder="Email" type="email" name="email" value="{{ old('email') }}">
+								<div class="ui loading form segment">
+									<form class="form-horizontal" role="form" method="POST" action="/auth/login">
+										<input type="hidden" name="_token" value="{{ csrf_token() }}">
+										<div class="field four-wide column">
+											<div class="field">
+												<label>Email</label>
+												<input placeholder="Email" type="email" name="email" value="{{ old('email') }}">
+											</div>
+											<div class="field">
+												<label>Password</label>
+												<input placeholder="Last Name" type="password" name="password">
+											</div>
 										</div>
-										<div class="field">
-											<label>Password</label>
-											<input placeholder="Last Name" type="password" name="password">
+										<div class="inline field">
+											<div class="ui checkbox">
+												<input type="checkbox" name="remember">
+												<label>Lembrar-me?</label>
+											</div>
 										</div>
-									</div>
-									<div class="inline field">
-										<div class="ui checkbox">
-											<input type="checkbox" name="remember">
-											<label>Lembrar-me?</label>
+										<div class="ui submit button">Login</div>
+											<a href="/password/email">Esqueceu a sua password?</a>
+									</form>
+									<div class="field"></div>
+									<div class="field">
+										<div class="ui facebook button">
+											<i class="facebook icon"></i>
+
 										</div>
-									</div>
-									<div class="ui submit button">Login</div>
-										<a href="/password/email">Esqueceu a sua password?</a>
-								</form>
-								<div class="field"></div>
-								<div class="field">
-									<div class="ui facebook button">
-										<i class="facebook icon"></i>
+										<div class="ui twitter button">
+											<i class="twitter icon	"></i>
 
-									</div>
-									<div class="ui twitter button">
-										<i class="twitter icon	"></i>
+										</div>
+										<div class="ui google plus button">
+											<i class="google plus icon"></i>
 
-									</div>
-									<div class="ui google plus button">
-										<i class="google plus icon"></i>
-
+										</div>
 									</div>
 								</div>
-							</div>
+						</div>
 					</div>
-				</div>
 			</section>
 		</div>
 				<!--LADO ESQUERDO REGISTAR-->
@@ -132,4 +132,5 @@
 		</section>
 	</div>
 </div>
+
 @endsection
